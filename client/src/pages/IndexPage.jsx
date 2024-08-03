1f4656ca3a0eba52e55ae9092265da859c2c7729
VizArcher <viztech2002@gmail.com>
2024-08-03 18:32:16 +0530

    Completed create post page by adding a post model in database and uploads to store images. Also added there api mehtods

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Post from "../Post";
 
 export default function IndexPage() {
+    useEffect(() => {
+        fetch('http://localhost:4000/post').then(response => {
+            response.json().then(posts => {
+                console.log(posts)
+            })
+        })
+    }, [])
     return (
         <>
             <Post />
@@ -8,4 +16,4 @@ export default function IndexPage() {
             <Post />
         </>
     )
-}
+}  
